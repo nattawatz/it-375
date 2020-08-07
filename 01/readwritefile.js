c9d627a69d5b4bbdbf635852b8512d7f34370c8d
kang-van-gent <610108020019@dpu.ac.th>
2020-08-07 15:22:20 +0700

    readwritefile

---
+++ 01/readwritefile.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+console.log(textIn);
+// Write File
+const textOut = `Avocado information:${textIn}\n Create on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log('File written');
